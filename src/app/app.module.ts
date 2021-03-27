f156a03cc8e1377f9a52424b64b8027d956ca938
Bryan Rodriguez <bryanrr_julio_@hotmail.com>
2021-03-27 12:18:00 -0600

    Add base login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DataModule } from "./data/data.module";
+import { LoginComponent } from './login/login.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
