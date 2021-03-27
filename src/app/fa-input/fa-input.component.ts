b4bc0c109e431158d0abbcbb74ff09819a5b6445
Bryan Rodriguez <bryanrr_julio_@hotmail.com>
2021-03-27 14:50:19 -0600

    Define HTML strucuture of custom input Note that in the template of the custom input will consist of a icon and a HTML input element, which will be specified using Content Projection. We apply a appSvgRef directive to the icon to set the focus and blur of the icon. It was also added bootstrap containers to control the position of the inner elements from this custom input.

--- src/app/fa-input/fa-input.component.ts
+++ src/app/fa-input/fa-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-fa-input',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fa-input.component.css']
 })
 export class FaInputComponent implements OnInit {
+
+  @Input() icon: string;
 
   constructor() { }
 
