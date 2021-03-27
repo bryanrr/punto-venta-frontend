ba8d2ac2e0ef5ad779f682566b76544944d67aca
Bryan Rodriguez <bryanrr_julio_@hotmail.com>
2021-03-27 14:57:48 -0600

    Synchronize blur and focus from elements We add a listener to hear on focus events on our custom input so that we can set the same events(propagete) to our our inner elements, which in this case are an icon and a HTML input element

--- src/app/fa-input/fa-input.component.ts
+++ src/app/fa-input/fa-input.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ContentChild, HostBinding, Input, OnInit, QueryList, ViewChildren } from '@angular/core';
+import { InputRefDirective } from '../directives/input-ref.directive';
+import { SvgRefDirective } from '../directives/svg-ref.directive';
 
 @Component({
   selector: 'app-fa-input',
@@ -9,7 +11,27 @@ export class FaInputComponent implements OnInit {
 
   @Input() icon: string;
 
+  @ContentChild(InputRefDirective)
+  input: InputRefDirective;
+
+  @ViewChildren(SvgRefDirective)
+  svg: QueryList<SvgRefDirective>;
+
   constructor() { }
+
+  @HostBinding("class.focus")
+  get focus() {
+    if(this.input&&this.svg){
+      this.svg.forEach(svgs=>{
+        if(svgs&&this.input.focus){
+          svgs.$focus=true;
+        }else{
+          svgs.$focus=false;
+        }
+      });
+    }
+    return this.input ? this.input.focus : false;
+  }
 
   ngOnInit(): void {
   }
