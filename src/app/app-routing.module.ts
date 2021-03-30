d014c823b447f6363ad4c232377204b81716671a
Bryan Rodriguez <bryanrr_julio_@hotmail.com>
2021-03-30 10:37:46 -0600

    Add logged-in guard into routing config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoggedInGuard } from './guards/logged-in.guard';
 
 import { LoginComponent } from './login/login.component';
 import { MainComponent } from './main/main.component';
 
 const routes: Routes = [
   {path:'login', component: LoginComponent},
-  {path:'main', component: MainComponent}
+  {path:'main', component: MainComponent,canActivate:[LoggedInGuard]}
 ];
 
 @NgModule({
