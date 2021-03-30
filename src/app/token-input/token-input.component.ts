91a367b217d600346c15f62cf0000aee0e9e247c
Bryan Rodriguez <bryanrr_julio_@hotmail.com>
2021-03-30 11:26:44 -0600

    Add token-input component

---
+++ src/app/token-input/token-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-token-input',
+  templateUrl: './token-input.component.html',
+  styleUrls: ['./token-input.component.css']
+})
+export class TokenInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
