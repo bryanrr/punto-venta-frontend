1490ca56c009d134bde5dd47544ec8821b0a23fe
Bryan Rodriguez <bryanrr_julio_@hotmail.com>
2021-03-31 15:33:52 -0600

    Add method to retrieve product from server
    As the retrieval of product takes time, in order to show the product,
    we'll use a Subject to show it the time the request has finished. We'll
    also save the retrieved object to let us decide whether or not to post
    it on a product update, by checkng first if there were actually changes.

--- src/app/data/productos.service.ts
+++ src/app/data/productos.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductosService {
+  
+  constructor(private httpClient:HttpClient) { }
 
-  constructor() { }
+  getProductDetails(code:string,producto:Subject<Object>):void{
+    this.httpClient.get<string>(environment.apiUrl+'producto/'+code,{responseType:'text' as 'json'})
+      .subscribe(data=>{
+        producto.next(JSON.parse(data)); 
+      },error=>{
+        
+      });
+  }
 }
+
+
--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { ProductosService } from '../data/productos.service';
 
 @Component({
   selector: 'app-product-details',
@@ -8,11 +10,17 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProductDetailsComponent implements OnInit {
   private codigo:string;
-
-  constructor(private route:ActivatedRoute) {
+  productObservable:Subject<Object>=new Subject();
+  product:Object;
+  
+  constructor(private route:ActivatedRoute,private productosService:ProductosService) {
     route.params.subscribe(params=>this.codigo=params['code']);
+    this.productosService.getProductDetails(this.codigo,this.productObservable);
+    this.productObservable.subscribe(data=>{
+      this.product=data;
+    });
   }
-
+  
 	public get $codigo(): string {
 		return this.codigo;
 	}
