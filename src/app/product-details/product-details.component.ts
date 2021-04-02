6312a0280d67efbfe3c253ce56ed7e0a44329da1
Bryan Rodriguez <bryanrr_julio_@hotmail.com>
2021-04-01 19:25:19 -0600

    Add HTML form and formgroup
    Values that can change will be bind to a formcontrol, while values that
    are read-only will be read from the object that keep the original state
    of the product.

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subject } from 'rxjs';
 import { ProductosService } from '../data/productos.service';
+import {FormBuilder,FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-product-details',
@@ -12,21 +13,32 @@ export class ProductDetailsComponent implements OnInit {
   private codigo:string;
   productObservable:Subject<Object>=new Subject();
   product:Object;
+  myForm:FormGroup;
   
-  constructor(private route:ActivatedRoute,private productosService:ProductosService) {
+  constructor(private route:ActivatedRoute,private productosService:ProductosService,private fb:FormBuilder) {
     route.params.subscribe(params=>this.codigo=params['code']);
     this.productosService.getProductDetails(this.codigo,this.productObservable);
     this.productObservable.subscribe(data=>{
       this.product=data;
+      this.myForm=this.fb.group({
+        'codigobarra':this.product['codigobarra'],
+        'descripcion':this.product['descripcion'],
+        'preciocompra':this.product['preciocompra'],
+        'precioventa':this.product['precioventa']
+      });
     });
   }
-  
+
+  onSubmit(values):void{
+    
+  }
+
 	public get $codigo(): string {
 		return this.codigo;
 	}
 
-
   ngOnInit(): void {
+    
   }
 
 }
