5fedc1f0a69c3166d5e632c2a632a85ad3d04c0e
Bryan Rodriguez <bryanrr_julio_@hotmail.com>
2021-04-03 14:58:45 -0600

    Fix validators service
    Regex expressions were changed from string to RegExp type as validations
    were failing. Also we forced to convert to string data type values from
    form controls as a value from a FormControl could be a number, if no
    letters were on the FormControl value, and a number varaible doesn't
    have either trim() or match() givinng null exception.
    On each FormControl validation it was excluded when the field was empty
    as this is validated by Validators.required.

--- src/app/data/validators.service.ts
+++ src/app/data/validators.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
 import { FormControl} from '@angular/forms';
 
-const regexObjects:Object={username:"/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/",
-  password:"/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/",
-  barcode:"/^(?=.{2,30}$)(?![ ])[a-zA-Z0-9]+(?<![_.])$/",
-  coincidencesProduct:"/^(?=.{2,50}$)[a-zA-Z0-9 ]+(?<![_.])$/",
-  price:"^[0-9]\d{0,4}(\.\d{0,2})?$"};
+const regexObjects:Object={username:/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/,
+  password:/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
+  barcode:/^(?=.{2,30}$)(?![ ])[a-zA-Z0-9]+(?<![_.])$/,
+  coincidencesProduct:/^(?=.{2,50}$)[a-zA-Z0-9 ]+(?<![_.])$/,
+  price:/^[0-9]\d{0,4}(\.\d{0,2})?$/};
 
 @Injectable({
   providedIn: 'root'
@@ -15,31 +15,36 @@ export class ValidatorsService {
   constructor() { }
 
   usernameValidator(control:FormControl):{[s:string]:boolean}{
-    if(!control.value.trim().match(regexObjects["username"])){
+    let efectiveValue=(control.value+"").trim();
+    if(!efectiveValue.match(regexObjects["username"]) && efectiveValue!=""){
       return {invalidUsername:true};
     }
   }
 
   passwordValidator(control:FormControl):{[s:string]:boolean}{
-    if(!control.value.trim().match(regexObjects["password"])){
+    let efectiveValue=(control.value+"").trim();
+    if(!efectiveValue.match(regexObjects["password"]) && efectiveValue!=""){
       return {invalidPassword:true};
     }
   }
 
   barcodeValidator(control:FormControl):{[s:string]:boolean}{
-    if(!control.value.trim().match(regexObjects["barcode"])){
+    let efectiveValue=(control.value+"").trim();
+    if(!efectiveValue.match(regexObjects["barcode"]) && efectiveValue!=""){
       return {invalidCode:true};
     }
   }
 
   coincidencesValidator(control:FormControl):{[s:string]:boolean}{
-    if(!control.value.trim().match(regexObjects["coincidencesProduct"])){
+    let efectiveValue=(control.value+"").trim();
+    if(!efectiveValue.match(regexObjects["coincidencesProduct"]) && efectiveValue!=""){
       return {invalidWords:true};
     }
   }
 
   priceValidator(control:FormControl):{[s:string]:boolean}{
-    if(!control.value.trim().match(regexObjects["price"])){
+    let efectiveValue=(control.value+"").trim();
+    if(!efectiveValue.match(regexObjects["price"]) && efectiveValue!=""){
       return {invalidPrice:true};
     }
   }
