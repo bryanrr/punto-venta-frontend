683d5caf8cd52fbc1c9be5d75b866374c6463893
Bryan Rodriguez <bryanrr_julio_@hotmail.com>
2021-04-03 12:25:51 -0600

    Add validators service
    Added a service that will provide the validators for each form control
    on all forms. By doing this we could reuse validators as well as regex
    expressions.

---
+++ src/app/data/validators.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { FormControl} from '@angular/forms';
+
+const regexObjects:Object={username:"/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/",
+  password:"/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/",
+  barcode:"/^(?=.{2,30}$)(?![ ])[a-zA-Z0-9]+(?<![_.])$/",
+  coincidencesProduct:"/^(?=.{2,50}$)[a-zA-Z0-9 ]+(?<![_.])$/"};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ValidatorsService {
+
+  constructor() { }
+
+  usernameValidator(control:FormControl):{[s:string]:boolean}{
+    if(!control.value.trim().match(regexObjects["username"])){
+      return {invalidUsername:true};
+    }
+  }
+
+  passwordValidator(control:FormControl):{[s:string]:boolean}{
+    if(!control.value.trim().match(regexObjects["password"])){
+      return {invalidPassword:true};
+    }
+  }
+
+  barcodeValidator(control:FormControl):{[s:string]:boolean}{
+    if(!control.value.trim().match(regexObjects["barcode"])){
+      return {invalidCode:true};
+    }
+  }
+
+  coincidencesValidator(control:FormControl):{[s:string]:boolean}{
+    if(!control.value.trim().match(regexObjects["coincidencesProduct"])){
+      return {invalidWords:true};
+    }
+  }
+
+}
