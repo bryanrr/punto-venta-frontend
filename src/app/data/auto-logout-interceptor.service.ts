6d0833b2da4a14b793f8f53f6587c4a235154551
Bryan Rodriguez <bryanrr_julio_@hotmail.com>
2021-08-03 15:47:20 -0500

    Add auto-logout mechanism
    Each 5 minutes we will check if the last date a request was made is more
    than 30 minutes ahead(this will show a 30 minutes inactivity), which if
    true, it logouts automatically

--- src/app/data/auto-logout-interceptor.service.ts
+++ src/app/data/auto-logout-interceptor.service.ts
@@ -1,16 +1,29 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { Observable, timer } from 'rxjs';
+import { LoginService } from './login.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AutoLogoutInterceptorService implements HttpInterceptor{
 
-  lastRequestDate:Date;
+  private lastRequestDate:Date;
+  private maxInactivityTime:number=1000*60*30;
+  private period:number=1000*60*5;
+  private initialDelay:number=1000*60*30;
   
-  constructor() { 
-    
+  constructor(private loginService:LoginService,private router:Router) { 
+    timer(this.initialDelay,this.period)
+    .subscribe(data=>{
+      if(this.loginService.$isLoggedIn && (( new Date().getTime()- this.lastRequestDate.getTime() ) >= this.maxInactivityTime)){
+        loginService.logout().
+          subscribe(data=>{
+            this.router.navigateByUrl('/login');
+          });
+      }
+    });
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
